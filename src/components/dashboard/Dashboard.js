a179d9f9d2d7a4f325630c86fc5cea19f9b43a73
Shivam Pandey <shivampandey12345678910@gmail.com>
2023-01-13 16:16:20 +0530

    slidefront added

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,7 +3,9 @@ import Notification from './Notification'
 import ProjectList from '../projects/ProjectList'
 import {compose} from 'redux'
 import { connect } from 'react-redux'
+import ImgMediaCard from '../projects/SlideFront'
 import { firestoreConnect } from 'react-redux-firebase'
+
 import {Navigate} from 'react-router-dom'
 class Dashboard extends Component {
   render() {
@@ -13,7 +15,10 @@ class Dashboard extends Component {
       <div className="dashboard container">
         <div className="row">
             <div className="col">
-                <ProjectList projects={projects}/>
+                {/* <ProjectList projects={projects}/> */}
+                <br></br>
+                <ImgMediaCard  text="burger"/>
+             
             </div>
             <div className="col offset-m1"></div>
             {/* <Notification/> */}
